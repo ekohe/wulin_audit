1d92f022e68c7f9984ceec69963fa50140cc3608
Maxime Guilbot <maxime@ekohe.com>
2011-12-06 00:14:22 +0800

    MISC - Added precompile instruction

--- lib/wulin_audit/engine.rb
+++ lib/wulin_audit/engine.rb
@@ -3,5 +3,8 @@ require "rails"
 module WulinAudit
   class Engine < Rails::Engine
     engine_name :wulin_audit
+    initializer "add assets to precompile" do |app|
+       app.config.assets.precompile += %w( audit.css )
+    end
   end
 end
