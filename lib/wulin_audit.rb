28ce625a34c2de0ebd8d6404345ad4420404b8c7
Xuhao <xuhao@rubyfans.com>
2012-04-05 11:12:30 +0800

    MISC - A little bit of corrections for wulin_audit

--- lib/wulin_audit.rb
+++ lib/wulin_audit.rb
@@ -3,6 +3,6 @@ require 'wulin_audit/engine' if defined? Rails
 
 module WulinAudit; end
 
-require 'wulin_audit/orm/active_record'
-require 'wulin_audit/orm/mongoid'
+require 'wulin_audit/orm/active_record' if defined? ActiveRecord
+require 'wulin_audit/orm/mongoid' if defined? Mongoid
 require 'wulin_audit/wulin_master' if defined? ::WulinMaster
