1e06d9504913c6368aa770805f1bd63a68281ae2
Xuhao <xuhao@rubyfans.com>
2013-10-08 14:57:00 +0800

    BUG #19612 @3h - Audit buttons on the grids should not be displayed if the user doesn't have the permission to see the global audit

--- lib/wulin_audit/wulin_master.rb
+++ lib/wulin_audit/wulin_master.rb
@@ -1,3 +1,14 @@
+# Override `action` method to append `:authorized?` option for `:audit` action
+class WulinMaster::Grid
+  def self.action(a_name, options={})
+    new_action = {name: a_name}.merge(options)
+    # append :authorized? option
+    new_action.reverse_merge!(:authorized? => lambda { |user| user.has_permission_with_name?('record_audit#read') }) if a_name.to_s == 'audit'
+    self.actions_pool << new_action
+    add_hotkey_action(a_name, options)
+  end
+end
+
 # Add audit button to every grid as default toolbar item
 WulinMaster::Grid.add_default_action "audit"
 
