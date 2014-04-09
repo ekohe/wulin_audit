3f73180da1aa50f94f1df8e8d851970835123e75
Maxime Guilbot <maxime@ekohe.com>
2014-04-10 00:18:15 +0800

    BUG #23933 - Add indexes to fix the indexing error when scrolling too deeply in the audit

--- app/models/wulin_audit/audit_log.rb
+++ app/models/wulin_audit/audit_log.rb
@@ -1,5 +1,13 @@
 module WulinAudit
   class AuditLog < ::ActiveRecord::Base
     reject_audit
+
+    index :created_at
+    index :user_email
+    index :action
+    index :class_name
+    index :record_id
+    index :request_ip
+    index :detail
   end
 end
