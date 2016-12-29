02e7d0b6bba684efd60f3070515808c296c956ca
Ben Zheng <ben@ekohe.com>
2016-12-28 18:59:49 -0800

    MISC - Update Migration
    - Update create_audit_log migration
    - Add engine db/migrate path to app.config.paths["db/migrate"]

--- db/migrate/20161202171600_create_audit_log.rb
+++ db/migrate/20161202171600_create_audit_log.rb
@@ -1,17 +1,15 @@
-class CreateAuditLog < ActiveRecord::Migration
+class CreateAuditLog < ActiveRecord::Migration[5.0]
   def change
-    unless table_exists?(:audit_logs)
-      create_table :audit_logs do |t|
-        t.integer :user_id
-        t.string  :request_ip
-        t.string  :user_email
-        t.string  :record_id
-        t.string  :action
-        t.string  :class_name
-        t.jsonb   :detail
-        t.timestamps
-      end
-      add_index :audit_logs, [:user_id, :record_id]
+    create_table :audit_logs do |t|
+      t.integer :user_id
+      t.string  :request_ip
+      t.string  :user_email
+      t.string  :record_id
+      t.string  :action
+      t.string  :class_name
+      t.jsonb   :detail
+      t.timestamps
     end
+    add_index :audit_logs, [:user_id, :record_id]
   end
 end
--- lib/wulin_audit/engine.rb
+++ lib/wulin_audit/engine.rb
@@ -3,8 +3,17 @@ require "rails"
 module WulinAudit
   class Engine < Rails::Engine
     engine_name :wulin_audit
+
     initializer "add assets to precompile" do |app|
        app.config.assets.precompile += %w( audit.css audit.js audit.png)
+    end
+
+    initializer :append_migrations do |app|
+      unless app.root.to_s.match root.to_s
+        config.paths["db/migrate"].expanded.each do |expanded_path|
+          app.config.paths["db/migrate"] << expanded_path
+        end
+      end
     end
   end
 end
