a1cbadc579e20b08592022215793d5e6cad1c9a2
Ben Zheng <ben@ekohe.com>
2017-08-15 14:52:09 -0700

    FEATURE #69819 - MD Implementation: Audit Logs

--- app/assets/javascripts/audit.js
+++ app/assets/javascripts/audit.js
@@ -3,48 +3,51 @@ WulinMaster.actions.Audit = $.extend({}, WulinMaster.actions.BaseAction, {
   name: 'audit',
 
   handler: function() {
-    // Remove all existing dialogs
-    $("#audit_dialog").remove();
-
-    var $dialog, $grid_container, currentGrid, selectedIds, recordUnit;
+    var self = this;
+    var $auditModal, $gridContainer, currentGrid, selectedIds, recordUnit;
 
     currentGrid = this.getGrid();
     selectedIds = currentGrid.getSelectedIds();
-    if (selectedIds.length > 0) {
-      recordUnit = selectedIds.length > 1 ? 'records' : 'record';
+    recordUnit = selectedIds.length > 1 ? 'records' : 'record';
 
-      $grid_container = $('<div/>')
-      .attr({'class': 'grid_record_audit'});
+    $gridContainer = $('<div/>').addClass('grid_record_audit');
 
+    var $auditModal = $('<div/>')
+      .attr({'id': 'audit_dialog'})
+      .addClass('modal modal-fixed-footer')
+      .css({overflow: 'hidden'})
+      .appendTo($('body'));
+    var $modalHeader = $('<div/>')
+      .addClass('modal-header')
+      .append($('<span/>').text('Audit logs for ' + recordUnit + ' with id: ' + selectedIds))
+      .append($('<i/>').text('close').addClass('modal-close material-icons right'))
+      .appendTo($auditModal);
+    var $modalContent = $('<div/>')
+      .addClass('modal-content')
+      .appendTo($auditModal);
+    var $modalFooter = $('<div/>')
+      .addClass('modal-footer')
+      .append($('<div/>').addClass('btn-flat modal-close').text('Cancel'))
+      .appendTo($auditModal);
 
-      $dialog = $('<div/>')
-      .attr({'id': 'audit_dialog', 'title': 'Audit logs for ' + recordUnit + ' with id: ' + selectedIds})
-      .css('display', 'none')
-      .append($grid_container)
-      .appendTo('body')
+    $auditModal.modal({
+      ready: function(modal, trigger) {
+        $.ajax({
+          type:'GET',
+          data: {record_ids: selectedIds.join(","), class_name: currentGrid.model},
+          url: '/wulin_audit/record_audits'
+        })
+        .success(function(data) {
+          $modalContent.html(data);
+          self.setGridHeightInModal($modalContent.parent());
+        });
+      },
+      complete: function() {
+        $auditModal.remove();
+      }
+    });
 
-      .dialog({
-        autoOpen: true,
-        width: 700,
-        height: 500,
-        buttons: {
-          "Ok": function() {
-            $(this).dialog("destroy");
-          }
-        },
-        modal: true,
-        create: function(event, ui) {
-          $.ajax({
-            type:'GET',
-            data: {record_ids: selectedIds.join(","), class_name:currentGrid.model},
-            url: '/wulin_audit/record_audits'
-          })
-          .success(function(data) { $grid_container.html(data); });
-        }
-      });
-    } else {
-      displayErrorMessage("Please select a record to see it's audit log.");
-    }
+    $auditModal.modal('open');
   }
 });
 
--- app/grid/record_audit_grid.rb
+++ app/grid/record_audit_grid.rb
@@ -3,18 +3,15 @@ if defined? WulinMaster
     title 'Record Audit Logs'
 
     model WulinAudit::AuditLog
-    
+
     path '/wulin_audit/record_audits'
 
     cell_editable false
-
-    action :filter
 
     def columns
       @columns = super.clone
       @columns.delete_if {|c| c.name.to_s =~ /^class_name$|^record_id$/}
       @columns
     end
-
   end
 end
