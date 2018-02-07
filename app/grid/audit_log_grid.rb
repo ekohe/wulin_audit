c90990ae6d9926706c0ffda2a187e41fcaf5ac81
Ben Zheng <ben@ekohe.com>
2018-02-06 17:26:20 -0800

    FEATURE #2 - Compatible with old wulin_master: Make filter & excel export work

--- app/grid/audit_log_grid.rb
+++ app/grid/audit_log_grid.rb
@@ -33,7 +33,7 @@ if defined? WulinMaster
     column :request_ip, :width => 150, :label => 'IP'
     column :detail, :width => 500
 
-    if WulinMaster::GridActions::ORIGINAL_ACTIONS
+    if defined? WulinMaster::GridActions::ORIGINAL_ACTIONS
       action :excel
       action :filter
     else
