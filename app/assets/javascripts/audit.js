b8524c3d446f3c1ca25a1ad10b640707a924814f
Ben Zheng <ben@ekohe.com>
2018-02-06 16:50:53 -0800

    FEATURE #2 - Compatible with old wulin_master: Make audit dialog/modal works in audit.js

--- app/assets/javascripts/audit.js
+++ app/assets/javascripts/audit.js
@@ -4,28 +4,57 @@ WulinMaster.actions.Audit = $.extend({}, WulinMaster.actions.BaseAction, {
 
   handler: function() {
     var self = this;
-    var $auditModal, $gridContainer, currentGrid, selectedIds, recordUnit;
+    var $gridContainer, currentGrid, selectedIds, recordUnit;
 
     currentGrid = this.getGrid();
     selectedIds = currentGrid.getSelectedIds();
     recordUnit = selectedIds.length > 1 ? 'records' : 'record';
-    modalTitle = 'Audit logs for ' + recordUnit + ' with id: ' + selectedIds;
+    title = 'Audit logs for ' + recordUnit + ' with id: ' + selectedIds;
+    ajaxOption = {
+      type:'GET',
+      data: {record_ids: selectedIds.join(','), class_name: currentGrid.model},
+      url: '/wulin_audit/record_audits'
+    }
+
+    if (selectedIds.length < 1) {
+      displayErrorMessage("Please select a record to see it's audit log.");
+      return false;
+    }
 
     $gridContainer = $('<div/>').addClass('grid_record_audit');
 
-    var $auditModal = Ui.headerModal(modalTitle, {
-      ready: function(modal, trigger) {
-        $.ajax({
-          type:'GET',
-          data: {record_ids: selectedIds.join(","), class_name: currentGrid.model},
-          url: '/wulin_audit/record_audits'
-        })
-        .success(function(data) {
-          modal.find('.modal-content').css('padding', '0').html(data);
-          self.setGridHeightInModal(modal);
+    if (typeof Ui.headerModal === 'function') {
+      Ui.headerModal(title, {
+        ready: function(modal, trigger) {
+          $.ajax(ajaxOption).success(function(data) {
+            modal.find('.modal-content').css('padding', '0').html(data);
+            self.setGridHeightInModal(modal);
+          });
+        }
+      });
+    } else {
+      $('<div/>')
+        .attr({'title': title})
+        .css('display', 'none')
+        .append($gridContainer)
+        .appendTo('body')
+        .dialog({
+          autoOpen: true,
+          width: 700,
+          height: 500,
+          buttons: {
+            'Ok': function() {
+              $(this).dialog('destroy');
+            }
+          },
+          modal: true,
+          create: function(event, ui) {
+            $.ajax(ajaxOption).success(function(data) { $gridContainer.html(data); });
+          }
         });
-      }
-    });
+
+    }
+
   }
 });
 
