17604686daff4f0daaed71acdf16416ee3c92fac
mel <mel@ekohe.com>
2026-08-03 11:33:12 +0800

    Guard the action_log migrations against databases that already ran them

--- db/migrate/20260723093000_add_user_email_index_to_action_logs.rb
+++ db/migrate/20260723093000_add_user_email_index_to_action_logs.rb
@@ -1,5 +1,6 @@
 class AddUserEmailIndexToActionLogs < ActiveRecord::Migration[5.0]
   def change
+    return if index_exists?(:action_logs, :user_email)
     add_index :action_logs, :user_email
   end
 end
